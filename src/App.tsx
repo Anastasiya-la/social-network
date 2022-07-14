4e1d047e8e8e02993dff672e026a701eafa8b902
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-07-14 23:36:35 +0300

    update

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,36 @@
 import React from 'react';
 import './App.css';
 
+
 function App() {
-  return (
-    <div className="App">
-     Hello, samurai! Let's go!
-    </div>
-  );
+    return (
+        <div className='app-wrapper'>
+            <header className='header'>
+                <img src="https://www.logogenie.net/download/preview/medium/9979471" alt="logo"/>
+            </header>
+            <nav className='nav'>
+                <div><a href="#s">Profile</a></div>
+                <div><a href="#s">Messages</a></div>
+                <div><a href="#s">News</a></div>
+                <div><a href="#s">Music</a></div>
+                <div><a href="#s">Settings</a></div>
+            </nav>
+            <div className='content'>
+                <div><img src="https://www.w3schools.com/howto/img_nature_wide.jpg"
+                          alt=""/></div>
+                <div>ava + description</div>
+                <div>
+                    My posts
+                    <div>New post</div>
+                    <div>
+                        <div>Post 1</div>
+                        <div>Post 2</div>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default App;
