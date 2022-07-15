0538b98cee4938ac26ada00845f5b48554df7144
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-07-15 15:15:02 +0300

    css-modules

--- src/components/Profile.tsx
+++ src/components/Profile.tsx
@@ -1,17 +1,18 @@
 import React from "react";
+import s from './Profile.module.css';
 
 const Profile = () => {
     return (
-        <div className='content'>
+        <div className={s.content}>
             <div><img src="https://www.w3schools.com/howto/img_nature_wide.jpg"
                       alt=""/></div>
             <div>ava + description</div>
             <div>
                 My posts
                 <div>New post</div>
-                <div>
-                    <div>Post 1</div>
-                    <div>Post 2</div>
+                <div className={s.posts}>
+                    <div className={s.item}>Post 1</div>
+                    <div className={s.item}>Post 2</div>
                 </div>
             </div>
 
