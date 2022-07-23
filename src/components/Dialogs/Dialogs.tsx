8b10bca06db63c00212c3d2647625abe63e3a293
Anastasiya Rabetskaya <nastia1362@gmail.com>
2022-07-23 19:48:20 +0300

    21-Dialogs layout

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -2,8 +2,19 @@ import s from './Dialogs.module.css';
 
 const Dialogs = () => {
     return (
-        <div>
-            Dialogs
+        <div className={s.dialogs}>
+            <div className={s.dialogsItems}>
+                <div className={s.dialog + ' ' + s.active}>Angelina</div>
+                <div className={s.dialog}>Vita</div>
+                <div className={s.dialog}>Katya</div>
+                <div className={s.dialog}>Veronika</div>
+                <div className={s.dialog}>Noemi</div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>Hi</div>
+                <div className={s.message}>How is your it-incubator?</div>
+                <div className={s.message}>Yo</div>
+            </div>
         </div>
     );
 }
