2e8d90502551598e6fa593c52079b06ee76640dc
Anastasiya Rabetskaya <nastia1362@gmail.com>
2023-04-14 16:53:56 +0300

    53 - class component

--- src/components/Users/Users.tsx
+++ src/components/Users/Users.tsx
@@ -1,53 +1,31 @@
 import React from 'react';
 import {UsersPropsType} from "./UsersContainer";
-import s from './Users.module.css';
+import s from "./Users.module.css";
+import userPhoto from "../../assets/images/ava.png";
 import axios from "axios";
-import userPhoto from '../../assets/images/ava.png'
+import {UserType} from "../../redux/users-reducer";
 
-const Users = (props: UsersPropsType) => {
-    if (props.users.length === 0) {
+class Users extends React.Component<any, any> {
+    constructor(props: UsersPropsType) {
+        super(props);
 
-        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => props.setUsers(response.data.items))
-        debugger
-        /* props.setUsers([
-             {
-                 id: 1,
-                 photoUrl: 'https://e7.pngegg.com/pngimages/565/216/png-clipart-smiley-emoticon-computer-icons-graphics-smiley-miscellaneous-face.png',
-                 followed: false,
-                 fullName: 'Angelina D.',
-                 status: 'Where your desire to be extraordinary?',
-                 location: {country: 'Belarus', city: 'Minsk'}
-             },
-             {
-                 id: 2,
-                 photoUrl: 'https://e7.pngegg.com/pngimages/565/216/png-clipart-smiley-emoticon-computer-icons-graphics-smiley-miscellaneous-face.png',
-                 followed: true,
-                 fullName: 'Dariya S.',
-                 status: 'Easy does it',
-                 location: {country: 'Ukraine', city: 'Kiev'}
-             },
-             {
-                 id: 3,
-                 photoUrl: 'https://e7.pngegg.com/pngimages/565/216/png-clipart-smiley-emoticon-computer-icons-graphics-smiley-miscellaneous-face.png',
-                 followed: false,
-                 fullName: 'Davide P.',
-                 status: 'Fed II',
-                 location: {country: 'Italy', city: 'Naples'}
-             }])*/
+        axios.get('https://social-network.samuraijs.com/api/1.0/users').then(response => this.props.setUsers(response.data.items))
     }
-    return (
-        <div>
-            {props.users.map(u => <div key={u.id}>
+
+    render() {
+        return (
+            <div>
+                {this.props.users.map((u: UserType) => <div key={u.id}>
                     <span>
                         <div><img className={s.userPhoto} src={u.photos.small ? u.photos.small : userPhoto}
                                   alt={'avatar'}/></div>
                         <div>
-                            {u.followed ? <button onClick={() => props.unfollow(u.id)}>Unfollow</button> :
-                                <button onClick={() => props.follow(u.id)}>Follow</button>}
+                            {u.followed ? <button onClick={() => this.props.unfollow(u.id)}>Unfollow</button> :
+                                <button onClick={() => this.props.follow(u.id)}>Follow</button>}
 
                         </div>
                     </span>
-                <span>
+                    <span>
                         <span>
                             <div>{u.name}</div>
                               <div>{u.status}</div>
@@ -58,9 +36,11 @@ const Users = (props: UsersPropsType) => {
                             <div>city</div>
                       </span>
                     </span>
-            </div>)}
-        </div>
-    )
+                </div>)}
+            </div>
+        );
+    }
+
 }
 
 export default Users;
